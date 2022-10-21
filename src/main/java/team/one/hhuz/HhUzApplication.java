e64eb20a0285ee20b1d45f2b5e0d82f447ad6ae9
mick <abdurashitovsh19@gmail.com>
2022-10-21 22:13:13 +0500

    Initial commit

---
+++ src/main/java/team/one/hhuz/HhUzApplication.java
@@ -0,0 +1,13 @@
+package team.one.hhuz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhUzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HhUzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/team/one/hhuz/HhUzApplicationTests.java
@@ -0,0 +1,13 @@
+package team.one.hhuz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhUzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
