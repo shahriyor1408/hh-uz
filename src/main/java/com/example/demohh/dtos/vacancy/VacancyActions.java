4b83a525f43c91d15a70220743f76833b0c27105
userhero21 <67582732+userhero21@users.noreply.github.com>
2022-10-22 17:36:26 +0500

    vacancy update

--- src/main/java/com/example/demohh/dtos/vacancy/VacancyActions.java
+++
@@ -1,4 +0,0 @@
-package com.example.demohh.dtos.vacancy;
-
-public interface VacancyActions {
-}
--- src/main/java/com/example/demohh/dtos/vacancy/VacancyCreateDTO.java
+++ src/main/java/com/example/demohh/dtos/vacancy/VacancyCreateDTO.java
@@ -11,7 +11,7 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class VacancyCreateDTO implements VacancyActions{
+public class VacancyCreateDTO{
     private String title;
     private int salaryFrom;
     private int salaryTo;
